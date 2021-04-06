8c38ba3b9f1286e8a028872aad17519a41602da2
SoulinxTBA <61413932+SoulinxTBA@users.noreply.github.com>
2021-04-06 12:44:10 +0800

    Add files via upload

---
+++ remCalc/remCalc/MainForm.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace remCalc
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		Double result = 0;
+		String opUsed = "";
+		bool activeOp = false;
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+		void num(object sender, EventArgs e)
+		{
+			Button userInput = (Button) sender;
+			if (textBox1.Text == "0" || (activeOp)){
+				textBox1.Clear();
+			}
+			if (userInput.Text == "."){
+				if (!textBox1.Text.Contains(".")){
+					textBox1.Text = textBox1.Text + userInput.Text;
+				}
+			}else
+			textBox1.Text = textBox1.Text + userInput.Text;
+			activeOp = false;
+		}
+		void opr(object sender, EventArgs e)
+		{
+			Button userInput = (Button) sender;
+			if (result != 0){
+				if (textBox1.Text == result.ToString()){
+					result = Double.Parse(textBox1.Text);
+				}
+				button16.PerformClick();
+				opUsed = userInput.Text;
+				activeOp = true;
+			}else{
+				opUsed = userInput.Text;
+				result = Double.Parse(textBox1.Text);
+				activeOp = true;
+			}
+			
+		}
+		void Button16Click(object sender, EventArgs e)
+		{
+			switch(opUsed){
+				case "+":
+					textBox1.Text = (result + Double.Parse(textBox1.Text)).ToString ();
+					break;
+				case "-":
+					textBox1.Text = (result - Double.Parse(textBox1.Text)).ToString ();
+					break;
+				case "*":
+					textBox1.Text = (result * Double.Parse(textBox1.Text)).ToString ();
+					break;
+				case "/":
+					textBox1.Text = (result / Double.Parse(textBox1.Text)).ToString ();
+					break;
+				default:
+					break;
+			}
+			result = Double.Parse(textBox1.Text);
+			activeOp = false;
+			if (textBox1.Text == result.ToString()){
+				result = Double.Parse(textBox1.Text);
+			}
+		}
+	}
+}
---
+++ remCalc/remCalc/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Acer
+ * Date: 03/03/2020
+ * Time: 10:22 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Windows.Forms;
+
+namespace remCalc
+{
+	/// <summary>
+	/// Class with program entry point.
+	/// </summary>
+	internal sealed class Program
+	{
+		/// <summary>
+		/// Program entry point.
+		/// </summary>
+		[STAThread]
+		private static void Main(string[] args)
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new MainForm());
+		}
+		
+	}
+}
